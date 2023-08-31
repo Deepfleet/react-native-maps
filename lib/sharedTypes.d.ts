b3a94d7a56e51a406f0e82179c4b32bd9cd8915b
Jijoy <jijoy21@gmail.com>
2023-08-31 12:56:46 +0530

    chore: merge

--- lib/sharedTypes.d.ts
+++ lib/sharedTypes.d.ts
@@ -1,94 +1,86 @@
-import {NativeSyntheticEvent} from 'react-native';
+import { NativeSyntheticEvent } from 'react-native';
 export type Provider = 'google' | undefined;
 export type MarkerPoints = LatLng & {
-  id: string;
-  title: string;
-  description: string;
+    id: string;
+    title: string;
+    description: string;
 };
 export type LatLng = {
-  latitude: number;
-  longitude: number;
+    latitude: number;
+    longitude: number;
 };
 export type Point = {
-  x: number;
-  y: number;
+    x: number;
+    y: number;
 };
 export type Region = LatLng & {
-  latitudeDelta: number;
-  longitudeDelta: number;
+    latitudeDelta: number;
+    longitudeDelta: number;
 };
 export type Frame = Point & {
-  height: number;
-  width: number;
+    height: number;
+    width: number;
 };
 export type CalloutPressEvent = NativeSyntheticEvent<{
-  action: 'callout-press';
-  /**
-   * @platform iOS
-   */
-  frame?: Frame;
-  /**
-   * @platform iOS
-   */
-  id?: string;
-  /**
-   * @platform iOS
-   */
-  point?: Point;
-  /**
-   * @platform Android
-   */
-  coordinate?: LatLng;
-  /**
-   * @platform Android
-   */
-  position?: Point;
+    action: 'callout-press';
+    /**
+     * @platform iOS
+     */
+    frame?: Frame;
+    /**
+     * @platform iOS
+     */
+    id?: string;
+    /**
+     * @platform iOS
+     */
+    point?: Point;
+    /**
+     * @platform Android
+     */
+    coordinate?: LatLng;
+    /**
+     * @platform Android
+     */
+    position?: Point;
 }>;
 export type LineCapType = 'butt' | 'round' | 'square';
 export type LineJoinType = 'miter' | 'round' | 'bevel';
-export type ClickEvent<T = {}> = NativeSyntheticEvent<
-  {
+export type ClickEvent<T = {}> = NativeSyntheticEvent<{
     coordinate: LatLng;
     position: Point;
-  } & T
->;
-export type MarkerDeselectEvent = Omit<
-  ClickEvent<{
+} & T>;
+export type MarkerDeselectEvent = Omit<ClickEvent<{
     action: 'marker-deselect';
     id: string;
-  }>,
-  'position'
->;
-export type MarkerSelectEvent = Omit<
-  ClickEvent<{
+}>, 'position'>;
+export type MarkerSelectEvent = Omit<ClickEvent<{
     id: string;
     action: 'marker-select';
-  }>,
-  'position'
->;
+}>, 'position'>;
 export type MarkerDragEvent = ClickEvent<{
-  /**
-   * @platform iOS
-   */
-  id?: string;
+    /**
+     * @platform iOS
+     */
+    id?: string;
 }>;
 export type MarkerDragStartEndEvent = NativeSyntheticEvent<{
-  coordinate: LatLng;
-  /**
-   * @platform iOS
-   */
-  id?: string;
-  /**
-   * @platform Android
-   */
-  position?: Point;
+    coordinate: LatLng;
+    /**
+     * @platform iOS
+     */
+    id?: string;
+    /**
+     * @platform Android
+     */
+    position?: Point;
 }>;
 export type MarkerPressEvent = NativeSyntheticEvent<{
-  id: string;
-  action: 'marker-press';
-  coordinate: LatLng;
-  /**
-   * @platform Android
-   */
-  position?: Point;
+    id: string;
+    action: 'marker-press';
+    coordinate: LatLng;
+    /**
+     * @platform Android
+     */
+    position?: Point;
 }>;
